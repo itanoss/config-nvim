a4c3864b8c59c978df78237de6884409b53902f8
Joonhyeok Im <itanoss@gmail.com>
2024-04-30 17:07:53 -0700

    Fix incremental selection failure

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,10 +1,5 @@
 return {
   {
-    "mrcjkb/rustaceanvim",
-    version = "^4", -- Recommended
-    ft = { "rust" },
-  },
-  {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.6",
     -- or                              , branch = '0.1.x',
@@ -19,5 +14,14 @@ return {
     config = function()
       require("refactoring").setup()
     end,
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
   },
 }
