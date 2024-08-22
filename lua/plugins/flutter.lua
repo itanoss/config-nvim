73ef574a5d949f2cafa73b21836d45ece240991f
Joonhyeok Im <itanoss@gmail.com>
2024-08-22 07:00:50 -0700

    Fix telescope issue

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,9 +1,22 @@
 return {
-  "akinsho/flutter-tools.nvim",
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "stevearc/dressing.nvim", -- optional for vim.ui.select
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = function()
+      require("flutter-tools").setup({
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+          exception_breakpoints = { "all" },
+        },
+        widget_guides = {
+          enabled = true,
+        },
+      })
+    end,
   },
-  config = true,
 }
